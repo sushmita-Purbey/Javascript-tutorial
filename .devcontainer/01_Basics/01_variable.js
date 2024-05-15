0d13db78f3a566afbd0795724c3d37e56e4226d4
sushmita-Purbey <165185693+sushmita-Purbey@users.noreply.github.com>
2024-05-15 08:18:20 +0000

    Variables

---
+++ .devcontainer/01_Basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "sushmitapurbey14@gmail.com"
+var accountPassword = "12563"
+accountCity = "Vadodara"
+let accountstate  // this gives output as undefined
+
+
+// accountId = 3  // we cannot update const
+
+accountEmail = "sumitpurey@123"
+accountPassword = "4637887934"
+accountCity = "Kolkata"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity,accountstate]);
