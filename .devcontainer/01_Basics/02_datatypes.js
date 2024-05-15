f1a7b0bbcefbf99a024bb41f71420f1fea6b68e4
sushmita-Purbey <165185693+sushmita-Purbey@users.noreply.github.com>
2024-05-15 08:18:36 +0000

    datatypes

---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //  treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// console.log(3+3)
+
+// console.log("Sushmita");
+
+
+let name = "Sushmita"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+// primitive datatypes
+// number => 2 to power 53
+// BigInt => for big numbers like in stock market n all
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof "Sushmita");
+
+console.log(typeof age);
+console.log(typeof null); //output comes as object
+console.log(typeof undefined);
