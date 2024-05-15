d95808e3d5cdaf194af4fb52f4cb64968653222a
sushmita-Purbey <165185693+sushmita-Purbey@users.noreply.github.com>
2024-05-15 11:11:32 +0000

    conversion

--- .devcontainer/01_Basics/03_conversionOperations.js
+++ .devcontainer/01_Basics/03_conversionOperations.js
@@ -27,11 +27,35 @@
 
 let someNumber = 56
 let stringNumber = String(someNumber);
-console.log(someNumber);
-console.log(typeof someNumber);
+// console.log(someNumber);
+// console.log(typeof someNumber);
 
 
 
 //1 => true; 0 => false
 // "" => false
 // "sushmita" => true
+
+//operations
+
+// let value = 5
+// let ngValue = -value
+// console.log(ngValue)
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2-2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Sushmita"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("2" + 3);//23
+console.log(3 + "1"); //31
+console.log("3" +2 + 4); //it gives 324
+console.log(1 + 2 + "3");  // it will add 1 and 2 that gives 3 and then ans will be 33
+console.log(true);
