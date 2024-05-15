9a7e7350d44663af4b07cccb2efc7ad3c1fac1a3
sushmita-Purbey <165185693+sushmita-Purbey@users.noreply.github.com>
2024-05-15 09:46:07 +0000

    conversion

---
+++ .devcontainer/01_Basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+
+// let score =77
+// console.log(typeof score)
+// console.log(typeof(score));
+
+
+// let score ="77nhjf"
+
+// let score = null
+// let score = true
+
+// console.log(typeof score)
+// console.log(typeof(score)); // it gives type as string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"77" => 77
+//"77jdh" => NaN
+// true => 1; false => 0
+// null => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "sushmita" => true
