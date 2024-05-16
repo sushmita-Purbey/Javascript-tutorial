bd00c84008774c8252395d6a138393b48f1903ae
sushmita-Purbey <165185693+sushmita-Purbey@users.noreply.github.com>
2024-05-16 09:04:42 +0000

    datatypes Summary

--- .devcontainer/01_Basics/datatypes-summary.js
+++ .devcontainer/01_Basics/datatypes-summary.js
@@ -2,36 +2,57 @@
 
 // 7 types : string , Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 200
-const scoreValue = 200.4
+// const score = 200
+// const scoreValue = 200.4
 
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123');
-console.log(id === anotherId);
+// const id = Symbol('123')
+// const anotherId = Symbol('123');
+// console.log(id === anotherId);
 
-// const bigNumbers = 62663556334566222n
+// // const bigNumbers = 62663556334566222n
 
-const heros =["Shaktiman", "nagraj", "doga"]
+// const heros =["Shaktiman", "nagraj", "doga"]
 
-let myObj = {
-    name : "Sushmita",
-    age : 22,
+// let myObj = {
+//     name : "Sushmita",
+//     age : 22,
+// }
+
+// const myFunction = function() {
+//     console.log("Hello World");
+// }
+// // console.log(typeof bigNumbers);
+// // console.log(typeof myFunction);
+// console.log(typeof outsideTemp);
+// console.log(typeof heros);
+// console.log(typeof anotherId);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+//stack(primitive) it is copy & heap(non primitive) it is reference
+
+let myYoutubename = "Sushmitapurbey.com"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
+let userTwo = userOne
 
-const myFunction = function() {
-    console.log("Hello World");
-}
-// console.log(typeof bigNumbers);
-// console.log(typeof myFunction);
-console.log(typeof outsideTemp);
-console.log(typeof heros);
-console.log(typeof anotherId);
+userTwo.email = "sushmita@google.com"
 
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
 
