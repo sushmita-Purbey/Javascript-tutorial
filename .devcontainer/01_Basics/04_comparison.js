a1f21c81781270cd80b64a8198a9fddcf645ffb1
sushmita-Purbey <165185693+sushmita-Purbey@users.noreply.github.com>
2024-05-16 05:21:45 +0000

    summary of datatypes

---
+++ .devcontainer/01_Basics/04_comparison.js
@@ -0,0 +1,7 @@
+// console.log(null > 0);  // we avoid this type of comparison that makes confusion
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);  //  whatever sign will be there undefined will give output as a false
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
---
+++ .devcontainer/01_Basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// primitive
+
+// 7 types : string , Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 200
+const scoreValue = 200.4
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123');
+console.log(id === anotherId);
+
+// const bigNumbers = 62663556334566222n
+
+const heros =["Shaktiman", "nagraj", "doga"]
+
+let myObj = {
+    name : "Sushmita",
+    age : 22,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+// console.log(typeof bigNumbers);
+// console.log(typeof myFunction);
+console.log(typeof outsideTemp);
+console.log(typeof heros);
+console.log(typeof anotherId);
+
+
+
+
+
+// Reference (Non primitive)
+
+//Array, objects, Functions
+
