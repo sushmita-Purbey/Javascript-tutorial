7a462c460e05a28f4d8cb59c61bb557fee9d8d85
sushmita-Purbey <165185693+sushmita-Purbey@users.noreply.github.com>
2024-05-17 05:35:03 +0000

    numbers and maths

---
+++ .devcontainer/01_Basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+// const score = 500
+// // console.log(score);
+
+// const balance = new  Number(100)
+// // console.log(balance);
+
+// // console.log(balance.toString().length);
+
+// // console.log(balance.toFixed(3)); // it gives  number after point
+
+// // const  otherNumber = 112.5677 //1.1e+2
+// const  otherNumber = 1112.5677
+
+// // console.log(otherNumber.toPrecision(4));
+// const hundreds = 10000000
+// // console.log(hundreds.toLocaleString('en-IN')); // 
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4)); // absolute value only convert -ve into +ve not like (- into + or + into - )
+// console.log(Math.round(36.4));
+// console.log(Math.ceil(4.5)); // top value it will choose
+// console.log(Math.floor(4.9)); // less value it will choose
+// console.log(Math.min(1,5,6,7,8));
+// console.log(Math.max(2,5,8,9,6));
+
+console.log(Math.random());
+
+console.log((Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))  + min);
